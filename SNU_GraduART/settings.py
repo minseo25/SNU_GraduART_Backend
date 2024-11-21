c28541042329b1673bc0b78bfccbc8d377a59a6d
minseo25 <ms059333@gmail.com>
2024-11-21 15:20:19 +0900

    fix: db를 fly.io의 persistent volume에 저장, persistent volumne mount

--- SNU_GraduART/settings.py
+++ SNU_GraduART/settings.py
@@ -95,7 +95,8 @@ WSGI_APPLICATION = 'SNU_GraduART.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
 }
 
